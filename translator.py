22541ea35edcc3bc28fbb4c04158efa052968a15
Vlad <vvlad1278@gmail.com>
2023-04-08 23:58:11 -0500

    Should be Good

--- translator.py
+++ translator.py
@@ -6,19 +6,15 @@ from deep_translator import GoogleTranslator
 
 # https://www.geeksforgeeks.org/user-input-in-pysimplegui/#
 sg.theme('LightBlue1')
-
-
 layout = [
     [sg.Text('Please enter the desired language and file to convert.')],
     [sg.Text('Language to Convert To:', size=(20, 1)), sg.InputText()],
     [sg.Text('File Location:', size=(20, 1)), sg.InputText()],
     [sg.Submit(), sg.Cancel()]
 ]
-
 window = sg.Window('Translator', layout)
 event, values = window.read()
 window.close()
-
 # end of gui code from geeksforgeeks
 language = values[0].lower()
 fileLocation = values[1].replace("\\", "\\\\").replace("\"", "")
@@ -29,8 +25,8 @@ translated = GoogleTranslator(source='auto', target=language).translate(original
 length = len(fileLocation)
 length = length - fileLocation.rindex("\\") - 1
 name = fileLocation[-length:]
-
 # GUI
+# https://stackoverflow.com/questions/64028319/pysimplegui-make-text-selectable-with-mouse for line right below
 layout = [[sg.InputText(translated, use_readonly_for_disable=True, disabled=True, key='-IN-')],
          [sg.Button('close'), sg.Button('copy')]]
 window = sg.Window("Translated Output", layout)
